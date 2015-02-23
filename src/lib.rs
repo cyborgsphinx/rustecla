8fc390f7b13f8a8fad9e058cf5d8797ad02a037a
James Hannah <jhannah8124@gmail.com>
2015-02-22 20:03:40 -0800

    The library is complete enough to allow the example shell to read a line and tab complete filenames. Gonna call that a win for a while

--- src/lib.rs
+++ src/lib.rs
@@ -1,46 +1,51 @@
+#![feature(libc)]
+#![feature(collections)]
+#![feature(std_misc)]
 extern crate libc;
 use libc::{c_char, c_int, size_t};
-use libc::types::common::c95::FILE;
+//use libc::types::common::c95::FILE;
 
-use std::ffi::CString;
+use std::ffi::{CString, CStr};
 use std::string::as_string;
 use std::str::from_utf8_unchecked;
+use std::ptr;
 
 #[repr(C)]
 pub struct GetLine;
-pub struct ExpandFile;
+/*pub struct ExpandFile;
 pub struct FileExpansion {
     exists: c_int,
     nfile: c_int,
     files: *mut char,
 }
 pub struct WordCompletion;
-
+*/
+// will add functions as needed
 #[link(name = "tecla")]
 extern {
-    fn libtecla_version(major: *mut c_int, minor: *mut c_int, micro: *mut c_int);
+    // fn libtecla_version(major: *mut c_int, minor: *mut c_int, micro: *mut c_int);
     fn new_GetLine(linelen: size_t, histlen: size_t) -> *mut GetLine;
-    fn del_GetLine(gl: *mut GetLine) -> *mut GetLine;
+    // fn del_GetLine(gl: *mut GetLine) -> *mut GetLine;
     fn gl_get_line(gl: *mut GetLine,
                    prompt: *const c_char,
                    start_line: *const c_char,
                    start_pos: c_int) -> *const c_char;
-    fn gl_query_char(gl: *mut GetLine, prompt: *const c_char, defchar: c_char) -> c_int;
-    fn gl_read_char(gl: *mut GetLine) -> c_int;
-    fn gl_configure_getline(gl: *mut GetLine, app_string: *const c_char,
-                            app_file: *const c_char, user_file: *const c_char) -> c_int;
+    // fn gl_query_char(gl: *mut GetLine, prompt: *const c_char, defchar: c_char) -> c_int;
+    // fn gl_read_char(gl: *mut GetLine) -> c_int;
+    // fn gl_configure_getline(gl: *mut GetLine, app_string: *const c_char,
+    //                        app_file: *const c_char, user_file: *const c_char) -> c_int;
     // fn gl_bind_keyseq() -> c_int;
-    fn new_ExpandFile() -> *mut ExpandFile;
-    fn del_ExpandFile(ef: *mut ExpandFile) -> *mut ExpandFile;
-    fn ef_expand_file(ef: *mut ExpandFile,
-                      path: *const c_char,
-                      pathlen: c_int) -> *mut FileExpansion;
-    fn el_list_expansions(result: *mut FileExpansion,
-                          fp: *mut FILE,
-                          term_width: c_int) -> c_int;
-    fn ef_last_error(ef: *mut ExpandFile) -> *const c_char;
-    fn new_WordCompletion() -> *mut WordCompletion;
-    fn del_WordCompletion(cpl: *mut WordCompletion) -> WordCompletion;
+    // fn new_ExpandFile() -> *mut ExpandFile;
+    // fn del_ExpandFile(ef: *mut ExpandFile) -> *mut ExpandFile;
+    // fn ef_expand_file(ef: *mut ExpandFile,
+    //                  path: *const c_char,
+    //                  pathlen: c_int) -> *mut FileExpansion;
+    // fn el_list_expansions(result: *mut FileExpansion,
+    //                      fp: *mut FILE,
+    //                      term_width: c_int) -> c_int;
+    // fn ef_last_error(ef: *mut ExpandFile) -> *const c_char;
+    // fn new_WordCompletion() -> *mut WordCompletion;
+    // fn del_WordCompletion(cpl: *mut WordCompletion) -> WordCompletion;
 }
 
 pub fn new_gl(linelen: usize, histlen: usize) -> *mut GetLine {
@@ -54,13 +59,12 @@ pub fn new_gl(linelen: usize, histlen: usize) -> *mut GetLine {
 }
 
 pub fn get_line(gl: *mut GetLine, prompt: &str) -> String {
-    let c_prompt = CString::from_slice(prompt.as_bytes());
-    let mut out: *const c_char;
-    let start = 0 as i8;
+    let c_prompt = CString::new(prompt.as_bytes()).unwrap();
+    let start: *const i8 = ptr::null();
     let mut res: &str;
     unsafe {
-        out = gl_get_line(gl, c_prompt.as_ptr(), &start, -1);
-        res = from_utf8_unchecked(std::ffi::c_str_to_bytes(&out));
+        let out = CStr::from_ptr(gl_get_line(gl, c_prompt.as_ptr(), start, -1));
+        res = from_utf8_unchecked(out.to_bytes());
     }
     as_string(res).clone()
 }
